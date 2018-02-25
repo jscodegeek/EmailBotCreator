4de28198aac6e298da8da7b1c334bf94cdc761a1
Eugene Safronov <evgeniy.safronov@outlook.com>
2018-02-25 20:09:46 +0200

    use proxy servers for webdriver, config and start working with mail.ru signup page

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const config = require('config');
 const winston = require('winston');
 const Guid = require('guid');
 
+const Chance = require('chance');
+const chance = new Chance();
+
+const {Builder, By, Key, until,} = require('selenium-webdriver');
+const proxy = require('selenium-webdriver/proxy');
+
 const appInstanceId = Guid.raw();
 
 const logPath = config.get('logs.filePath');
@@ -14,18 +20,20 @@ const logger = new (winston.Logger)({
 
 logger.info(`EmailBotCreator started, instanceId: ${appInstanceId}`);
 
+const proxyOptions = proxy.manual({
+  https : chance.pickone(config.get('proxy.https'))
+}); 
 
-//Check that webdriver and chrome package are working properly
-
-// const {Builder, By, Key, until} = require('selenium-webdriver');
-
-// (async function example() {
-//   let driver = await new Builder().forBrowser('chrome').build();
-//   try {
-//     await driver.get('http://www.google.com/ncr');
-//     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-//     await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-//   } finally {
-//     await driver.quit();
-//   }
-// })();
+(async function example() {
+  let driver = await new Builder()
+    .forBrowser('chrome')
+    .setProxy(proxyOptions)
+    .build();
+  logger.info(driver);
+  try {
+    await driver.get('https://account.mail.ru/signup');
+    await driver.wait(until.titleIs('webdriver - Google Search'), 100000000);
+  } finally {
+    await driver.quit();
+  }
+})();
